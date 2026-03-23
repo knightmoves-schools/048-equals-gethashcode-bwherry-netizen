fd1329b66a0369215e188754c0c771f037e9e294
Brendon W. <bwherry@midland.k12.ia.us>
2026-03-23 12:19:00 -0500

    Update Car.cs

--- Car.cs
+++ Car.cs
@@ -1,36 +1,43 @@
-﻿namespace knightmoves;
+namespace knightmoves;
 
-public class Car{
-    public string Make {get;}
-    public string Model {get;}
+public class Car
+{
+    public string Make { get; }
+    public string Model { get; }
 
     public Car(string make, string model)
     {
         Make = make;
         Model = model;
     }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Car other)
+        {
+            return Make == other.Make && Model == other.Model;
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Make, Model);
+    }
+
+    public static bool operator ==(Car first, Car second)
+    {
+        if (ReferenceEquals(first, second))
+            return true;
+
+        if (first is null || second is null)
+            return false;
+
+        return first.Make == second.Make && first.Model == second.Model;
+    }
+
+    public static bool operator !=(Car first, Car second)
+    {
+        return !(first == second);
+    }
 }
-
-Car firstCar = new Car ("Ford", "Camry");
-Car secondCar = new Car ("Ford", "Camry");
-
-public override bool Equals (object obj){
-    return obj is Car && this == (Car) obj;
-}
-
-public override int getHashCode(){
-   return HashCode.Combine(Make, Model);
-}
-
-public static bool operator ==(Car first, Car second){
-    return first.Make == second.Make && first.Model == second.Model;
-}
-
-public static bool operator !=(Car first, Car second){
-    return !(first == second);
-}
-
-
-
-
-
