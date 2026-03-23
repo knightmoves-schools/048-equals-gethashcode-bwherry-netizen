a32fb05795b5c9ad769bb0630ac4e13ab792d2f9
Brendon W. <bwherry@midland.k12.ia.us>
2026-03-23 12:16:20 -0500

    Update Car.cs

--- Car.cs
+++ Car.cs
@@ -10,3 +10,27 @@ public class Car{
         Model = model;
     }
 }
+
+Car firstCar = new Car ("Ford", "Camry");
+Car secondCar = new Car ("Ford", "Camry");
+
+public override bool Equals (object obj){
+    return obj is Car && this == (Car) obj;
+}
+
+public override int getHashCode(){
+   return HashCode.Combine(Make, Model);
+}
+
+public static bool operator ==(Car first, Car second){
+    return first.Make == second.Make && first.Model == second.Model;
+}
+
+public static bool operator !=(Car first, Car second){
+    return !(first == second);
+}
+
+
+
+
+
